65a046c7a3ed64dbfae89ae1ed0f24e68c16accd
gdarko <dg@darkog.com>
2020-09-26 18:47:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WP Validation
+
+Extends the PHP Validation package to provide WordPress specific validation rules.
---
+++ src/Validator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace IgniteKit\WP\Validation;
+
+use IgniteKit\Validation\Validator as BaseValidator;
+
+/**
+ * Class Validator
+ * @package IgniteKit\WP\Validation
+ */
+class Validator extends BaseValidator { }
